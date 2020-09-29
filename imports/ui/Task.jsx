868de0f4eded48803ac70acf978c72b8e0682691
AshutoshSajan <ashutoshsajan1213@gmail.com>
2020-09-29 11:24:25 +0530

    minor changes

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -9,7 +9,7 @@ export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
                 onClick={() => onCheckboxClick(task)}
                 readOnly
             />
-            <span>{task.text}</span>
+            <span className={task.isChecked ? "complete" : ""}>{task.text}</span>
             <button onClick={() => onDeleteClick(task)}>&times;</button>
         </li>
     );
