57af69e8c3d959352d57e3f2a24fbb5c8309f73b
AshutoshSajan <ashutoshsajan1213@gmail.com>
2020-10-01 12:52:26 +0530

    check

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,3 +1,4 @@
+// main entry app
 import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 // import { TasksCollection } from '/imports/api/TasksCollection';
